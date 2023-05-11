a29c4341936ff9025072bf138f1c6c5b6f304513
Lirys <iamyourmarkieboo@gmail.com>
2023-05-12 01:10:03 +0800

    Modified Users.java to reflect its prototype design

--- src/UserInterfaces/CustomComponents/PageableComponents/Users.java
+++ src/UserInterfaces/CustomComponents/PageableComponents/Users.java
@@ -1,13 +1,28 @@
 package UserInterfaces.CustomComponents.PageableComponents;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class Users extends JPanel {
     public static final String NAME = "Users";
     public Users(){
-        this.add(new JLabel("Insert Users tab here"));
         this.setName(NAME);
-        this.setLayout(new GridBagLayout());
+        this.setLayout(new BorderLayout());
+        JTable table = new JTable(5,5);
+        JScrollPane scrollPane = new JScrollPane(table);
+        JPanel testPane = new JPanel();
+        TitledBorder border = new TitledBorder("Customer");
+
+        this.add(scrollPane, BorderLayout.CENTER);
+
+        this.add(testPane, BorderLayout.EAST);
+        border.setTitlePosition(TitledBorder.TOP);
+        testPane.setBorder(border);
+        testPane.setPreferredSize(new Dimension(750,0));
+
+        testPane.add(new JButton("Ass"));
+        testPane.add(new JButton("Ass"));
+        testPane.add(new JButton("Ass"));
     }
 }
