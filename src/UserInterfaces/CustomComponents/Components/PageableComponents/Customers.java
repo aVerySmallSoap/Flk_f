b7ca790c16760a85cdf3008a24bc6da36d335e3a
Lirys <iamyourmarkieboo@gmail.com>
2023-05-13 00:57:50 +0800

    Renamed from Users.java to Customers.java

---
+++ src/UserInterfaces/CustomComponents/Components/PageableComponents/Customers.java
@@ -0,0 +1,42 @@
+package UserInterfaces.CustomComponents.Components.PageableComponents;
+
+import UserInterfaces.CustomComponents.Colors;
+import UserInterfaces.CustomComponents.Components.CustomPanels.CustomerRegistration;
+import UserInterfaces.CustomComponents.Components.CustomPanels.SearchPanel;
+import UserInterfaces.CustomComponents.Themes.SplitTheme;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
+
+public class Customers extends JPanel {
+    public static final String NAME = "Customers";
+    public Customers(){
+        SplitTheme.setUp();
+        this.setName(NAME);
+        this.setLayout(new BorderLayout());
+        JTable table = new JTable(5,5);
+        JScrollPane scrollPane = new JScrollPane(table);
+        JPanel tablePanel = new JPanel(new BorderLayout());
+        JPanel tableBottomPadding = new JPanel();
+        JSplitPane split = new JSplitPane();
+        CustomerRegistration customer = new CustomerRegistration();
+
+        split.setResizeWeight(1);
+        this.add(split, BorderLayout.CENTER);
+
+        tablePanel.add(new SearchPanel(), BorderLayout.NORTH);
+        scrollPane.setBorder(new EmptyBorder(1,1,1,1));
+        tablePanel.add(scrollPane, BorderLayout.CENTER);
+        tablePanel.add(tableBottomPadding, BorderLayout.SOUTH);
+        tableBottomPadding.add(new JLabel("Test pad"));
+        tableBottomPadding.setBorder(new EmptyBorder(100,0,0,0));
+        tablePanel.setMinimumSize(new Dimension(650,0));
+        tablePanel.setBorder(new MatteBorder(0,0,0,1, Colors.PRIMARY));
+        split.setLeftComponent(tablePanel);
+
+
+        split.setRightComponent(customer);
+    }
+}
