d0049d34265c82bda434036d6bcaec16cdab0f75
Lirys <iamyourmarkieboo@gmail.com>
2023-05-12 09:32:15 +0800

    Modified Users.java to reflect its prototype design

--- src/UserInterfaces/CustomComponents/PageableComponents/Users.java
+++ src/UserInterfaces/CustomComponents/PageableComponents/Users.java
@@ -1,5 +1,7 @@
 package UserInterfaces.CustomComponents.PageableComponents;
 
+import UserInterfaces.CustomComponents.Themes.SplitTheme;
+
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
@@ -7,22 +9,27 @@ import java.awt.*;
 public class Users extends JPanel {
     public static final String NAME = "Users";
     public Users(){
+        SplitTheme.setUp();
         this.setName(NAME);
         this.setLayout(new BorderLayout());
         JTable table = new JTable(5,5);
         JScrollPane scrollPane = new JScrollPane(table);
         JPanel testPane = new JPanel();
+        JPanel tablePanel = new JPanel(new GridLayout());
         TitledBorder border = new TitledBorder("Customer");
+        JSplitPane split = new JSplitPane();
 
-        this.add(scrollPane, BorderLayout.CENTER);
+        this.add(split, BorderLayout.CENTER);
 
-        this.add(testPane, BorderLayout.EAST);
+        tablePanel.setPreferredSize(new Dimension(500,0));
+        tablePanel.add(scrollPane);
+        split.setLeftComponent(tablePanel);
+
         border.setTitlePosition(TitledBorder.TOP);
         testPane.setBorder(border);
-        testPane.setPreferredSize(new Dimension(750,0));
-
         testPane.add(new JButton("Ass"));
         testPane.add(new JButton("Ass"));
         testPane.add(new JButton("Ass"));
+        split.setRightComponent(testPane);
     }
 }
